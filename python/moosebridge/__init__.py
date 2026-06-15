265350cace76360f4ee0feb75f31565881b22607
Frank <frank@inter-zone.de>
2026-06-15 14:01:41 +0200

    Export public Python bridge API

--- python/moosebridge/__init__.py
+++ python/moosebridge/__init__.py
@@ -1,1 +1,15 @@
 """MOOSE Bridge Python package."""
+
+from .protocol import BridgeCommand, BridgeMessage
+from .server import MooseBridgeServer
+from .sdk import MooseBridgeClient
+from .state import MooseBridgeState, MooseObjectIdentity
+
+__all__ = [
+    "BridgeCommand",
+    "BridgeMessage",
+    "MooseBridgeClient",
+    "MooseBridgeServer",
+    "MooseBridgeState",
+    "MooseObjectIdentity",
+]
