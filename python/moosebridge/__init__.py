55f9c8a29195b1bec22a417c4de54ed880a4c2ac
Frank <frank@inter-zone.de>
2026-06-16 14:31:36 +0200

    Export typed OPS snapshot models

--- python/moosebridge/__init__.py
+++ python/moosebridge/__init__.py
@@ -1,15 +1,20 @@
 """MOOSE Bridge Python package."""
 
+from .models import Auftrag, MooseSnapshotObject, OpsGroup, OpsZone
 from .protocol import BridgeCommand, BridgeMessage
 from .server import MooseBridgeServer
 from .sdk import MooseBridgeClient
 from .state import MooseBridgeState, MooseObjectIdentity
 
 __all__ = [
+    "Auftrag",
     "BridgeCommand",
     "BridgeMessage",
     "MooseBridgeClient",
     "MooseBridgeServer",
     "MooseBridgeState",
     "MooseObjectIdentity",
+    "MooseSnapshotObject",
+    "OpsGroup",
+    "OpsZone",
 ]
