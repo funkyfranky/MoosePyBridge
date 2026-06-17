aeee22b763729854600d52ffde2099530a0a559d
Frank <frank@inter-zone.de>
2026-06-17 10:13:16 +0200

    Export platform category matching helpers

--- python/moosebridge/__init__.py
+++ python/moosebridge/__init__.py
@@ -1,6 +1,12 @@
 """MOOSE Bridge Python package."""
 
-from .auftrag_specs import AuftragParameterSpec, AuftragTypeSpec, get_auftrag_type_spec
+from .auftrag_specs import (
+    AuftragParameterSpec,
+    AuftragTypeSpec,
+    expand_platform_categories,
+    get_auftrag_type_spec,
+    platform_categories_match,
+)
 from .legions import Cohort, CohortSummary, Legion
 from .models import Auftrag, MooseSnapshotObject, OpsGroup, OpsZone, TargetObjectSnapshot, TargetSnapshot
 from .protocol import BridgeCommand, BridgeMessage
@@ -26,5 +32,7 @@ __all__ = [
     "OpsZone",
     "TargetObjectSnapshot",
     "TargetSnapshot",
+    "expand_platform_categories",
     "get_auftrag_type_spec",
+    "platform_categories_match",
 ]
